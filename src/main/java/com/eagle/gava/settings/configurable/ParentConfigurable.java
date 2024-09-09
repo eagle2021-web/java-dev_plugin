7f6efa95775ccc3a136a5a18ee1c2a36162b572c
eagle2021-web <chenwangying16@163.com>
2024-09-10 00:36:26 +0800

    分层尝试 成功2

--- src/main/java/com/eagle/gava/settings/configurable/ParentConfigurable.java
+++ src/main/java/com/eagle/gava/settings/configurable/ParentConfigurable.java
@@ -37,32 +37,42 @@ public class ParentConfigurable implements Configurable, SearchableConfigurable 
     public JComponent createComponent() {
         myPanel = new JPanel(new GridBagLayout());
         GridBagConstraints gbc = new GridBagConstraints();
-
+        int lineIdx = 0;
         // 添加 helloLabel
         helloLabel = new JLabel("hello");
         gbc.gridx = 0; // 列索引
-        gbc.gridy = 0; // 行索引
+        gbc.gridy = lineIdx; // 行索引
         gbc.weightx = 1; // 水平方向占用比例
         gbc.weighty = 0; // 垂直方向占用比例
         gbc.fill = GridBagConstraints.HORIZONTAL; // 水平方向填充
         myPanel.add(helloLabel, gbc);
+        lineIdx++;
+
+        gbc.gridy = lineIdx; // 行索引
+        myPanel.add(new JPanel(), gbc);
+        lineIdx++;
 
         // 添加 linkLabel1
         linkLabel1 = new LinkLabel("https://www.example.com", null, (aSource, aLinkData) -> {
         });
-        gbc.gridy = 1; // 行索引
+        JPanel jPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        Component add = jPanel.add(new JLabel("  "));
+        Component add1 = jPanel.add(linkLabel1);
+
+        gbc.gridy = lineIdx; // 行索引
         gbc.weighty = 0; // 垂直方向占用比例
-        myPanel.add(linkLabel1, gbc);
+        myPanel.add(jPanel, gbc);
+        lineIdx++;
 
         // 添加 linkLabel2
         linkLabel2 = new LinkLabel("sss", null, (aSource, aLinkData) -> {
         });
-        gbc.gridy = 2; // 行索引
+        gbc.gridy = lineIdx; // 行索引
         gbc.weighty = 0; // 垂直方向占用比例
         myPanel.add(linkLabel2, gbc);
-
+        lineIdx++;
         // 设置底部空白填充
-        gbc.gridy = 3; // 行索引
+        gbc.gridy = lineIdx; // 行索引
         gbc.weighty = 1; // 垂直方向占用比例
         gbc.fill = GridBagConstraints.BOTH; // 填充整个区域
         myPanel.add(new JPanel(), gbc); // 添加一个空的面板以填充底部空间
@@ -89,6 +99,7 @@ public class ParentConfigurable implements Configurable, SearchableConfigurable 
     public void disposeUIResources() {
 
     }
+
     @Override
     public @NotNull @NonNls String getId() {
         return "com.eagle.gava.settings.configurable.ParentConfigurable";
