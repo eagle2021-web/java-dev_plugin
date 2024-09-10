6bc9d89a8585a0abfdcfe30028c6d7bbbe174de7
eagle2021-web <chenwangying16@163.com>
2024-09-10 23:25:16 +0800

    333

--- src/main/java/com/eagle/gava/settings/panel/LightPanel.java
+++ src/main/java/com/eagle/gava/settings/panel/LightPanel.java
@@ -31,44 +31,53 @@ public class LightPanel {
         return jPanel;
     }
 
-    public void init() {
-        root = new JPanel(new GridBagLayout());
-        JPasswordField passwordField = new JPasswordField();
-        GridBagConstraints gbc = new GridBagConstraints();
-        gbc.gridx = 0; // 列索引
-        gbc.gridy = 0;
-        gbc.weightx = 1; // 水平方向占用比例
-        gbc.weighty = 0; // 垂直方向占用比例
+    protected void appendLineComp(GridBagConstraints gbc, JPanel root, String text, JTextField component) {
         gbc.anchor = GridBagConstraints.WEST;
-        gbc.fill = GridBagConstraints.HORIZONTAL; // 水平方向填充
-        JTextField usernameField = new JTextField();
-
-//        JComponent leftPanel = createLeftPanel("用户名   是对方", usernameField);
-//        root.add(leftPanel, gbc);
-//        gbc.gridy++;
-//        JComponent passwordPanel = createLeftPanel("密码：", passwordField);
-//        root.add(passwordPanel, gbc);
-
-//        gbc.gridy++;
-//        gbc.anchor = GridBagConstraints.WEST;
+        gbc.fill = GridBagConstraints.WEST;
+        JLabel jLabel = new JLabel(text);
+        jLabel.setPreferredSize(new Dimension(60, jLabel.getPreferredSize().height));
         gbc.gridx = 0;
-        gbc.weightx = 0.2;
-        JLabel jLabel = new JLabel("第三行");
-        jLabel.setPreferredSize(new Dimension(220, 30));
+        gbc.weightx = 0.1;
         root.add(jLabel, gbc);
+
         gbc.gridx = 1;
         gbc.weightx = 0.3;
-        Component textField = root.add(new JTextField());
-        root.add(textField, gbc);
-        gbc.gridx = 2;
-        gbc.weightx = 1;
-        gbc.weightx = 0.4;
-        gbc.fill = GridBagConstraints.BOTH; // 水平方向填充
+        // 设置固定宽度为 100 像素
+//        component.setPreferredSize(new Dimension(100, component.getPreferredSize().height));
+        component.setColumns(20);
+        root.add(component, gbc);
+
+        gbc.fill = GridBagConstraints.REMAINDER;
+        gbc.weightx = 0.5;
         root.add(new JPanel(), gbc);
+    }
+
+    public void init() {
+        root = new JPanel(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.weightx = 0;
+        gbc.weighty = 0;
+        JTextField jTextField = new JTextField();
+        appendLineComp(gbc, root, "111", jTextField);
 
         gbc.gridy++;
-        gbc.weighty = 1; // 垂直方向占用比例
-        gbc.fill = GridBagConstraints.BOTH; // 填充整个区域
-        root.add(new JPanel(), gbc); // 添加一个空的面板以填充底部空间
+        JTextField jTextField2 = new JTextField();
+        appendLineComp(gbc, root, "111222222", jTextField2);
+
+        gbc.gridy++;
+        JTextField jTextField3 = new JTextField();
+        appendLineComp(gbc, root, "111222222", jTextField3);
+
+        gbc.gridy++;
+        JPasswordField jPasswordField = new JPasswordField();
+        appendLineComp(gbc, root, "密碼：", jPasswordField);
+        jPasswordField.setColumns(30);
+
+        gbc.gridy++;
+        gbc.weighty = 1;
+        gbc.fill = GridBagConstraints.BOTH;
+        root.add(new JPanel(), gbc);
     }
 }
